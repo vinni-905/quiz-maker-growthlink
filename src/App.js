1a85918a34f4176604a8fc35efd2357adb4de5b0
vinni-905 <likhithachinthirala@gmail.com>
2025-04-25 20:20:24 +0530

    Fixed issues in HomePage and added missing components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-// src/App.js
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from './components/HomePage'; // Make sure to use HomePage consistently
 import Navbar from './components/Navbar';
 import QuizPage from './components/QuizPage';
 import ResultPage from './components/ResultPage';
@@ -10,9 +10,10 @@ import Questions from './components/Questions';
 function App() {
   return (
     <Router>
-      <Navbar /> {/* Display Navbar at the top */}
+      <Navbar /> {/* Navbar at the top */}
       <div className="mt-16"> {/* To avoid navbar overlap */}
         <Routes>
+          <Route path="/" element={<HomePage />} /> {/* Use HomePage component name here */}
           <Route path="/quiz" element={<QuizPage />} />
           <Route path="/questions" element={<Questions />} />
           <Route path="/leaderboard" element={<LeaderboardPage />} />
